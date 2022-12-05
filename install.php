f819a89c119f91688bd3a1944161632066ef2097
jomabal98 <56680477+jomabal98@users.noreply.github.com>
2022-12-05 12:47:35 +0100

    Create install.php
    instance class db

---
+++ install.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__ . '/src/Db.php';
+if (!$db->checkTable($atr)) {
+    echo $db->getLastError();
+}
+
+$data = file_get_contents("src/data.json");
+$data = json_decode($data, true);
+$ins = [];
+$query = '';
+foreach ($data as $event) {
+    $ins = [];
+    foreach ($event as $key => $values) {
+        $ins += [$key => $values];
+    }
+
+    $query .= $db->getQuery('INSERT', $ins);
+}
+
+if ($query == false) {
+    die($db->getLastError());
+}
+
+$resultado = $db->executeS($query);
+if ($resultado) {
+    echo "Consulta correctamente en " . $db->getTable() . "<hr>";
+} else {
+    echo $db->getLastError();
+}
